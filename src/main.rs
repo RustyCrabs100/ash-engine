72975f9163cfdd143791ec3fc57888dfb154d806
RustyCrabs100 <rusty.crabs100@gmail.com>
2025-07-10 06:53:02 +0300

    Improved Error Handling + Deallocated Vulkan Items

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,12 @@ pub(crate) mod vulkan;
 
 fn main() {
     let vulkan_setup_items = vulkan::vulkan::VulkanInit::new();
+    match vulkan_setup_items {
+        Ok(vulkan_setup) => {
+            println!("Vulkan Setup Successfully Completed");
+            vulkan::vulkan::VulkanInit::clean_up(vulkan_setup);
+        },
+        Err(vulkan_failure) => panic!("Vulkan Setup failed, no other option currently avaliable"),
+    }
+    
 }
